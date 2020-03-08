1831201a0376d7f502e97e5fb237e68343ace7e6
BOINISHIVANANDAM <61923913+BOINISHIVANANDAM@users.noreply.github.com>
2020-03-08 15:00:01 +0530

    changes

---
+++ client.php
@@ -0,0 +1,10 @@
+<?php
+$a=$_POST["t1"];
+$b=$_POST["t2"];
+$c=$_POST["t3"];
+$d=$_POST["t4"];
+echo"Your first name is :".$a ."<br>";
+echo"Your last name is :".$b ."<br>";
+echo"Your email id is : ".$c ."<br>";
+echo"Your password is : ".$d;
+?>
